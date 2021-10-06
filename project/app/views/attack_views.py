6ab7d49a82d8d8849ad00b3632483a7232462387
mj <meanjunghong@naver.com>
2021-10-06 12:57:58 +0900

    [mj] update ssploit & add nmap

--- project/app/views/attack_views.py
+++ project/app/views/attack_views.py
@@ -10,13 +10,19 @@ import datetime
 from app.modules import loggers, statusCode, parser, sckt_utils, cmd_setter
 logger = loggers.create_logger(__name__)
 
+from app import MyIP
+from private.ports import WEB_SERVER_PORT
+
+CURRENT_DIR = os.path.abspath(os.getcwd())
+downloadURL = f"http://{MyIP}:{WEB_SERVER_PORT}/ssploit/download"
+
 
 bp = Blueprint('attack', __name__, url_prefix='/attack')
 
 
 @bp.route('/filter')
 def attack_filter():
-    type1 = request.args.get('type1') # product, endpoint, malware
+    type1 = request.args.get('type1') # product, endpoint, malware, product_packet
     src_ip = request.args.get('src_ip')
     dst_ip = request.args.get('dst_ip')
     
@@ -29,26 +35,29 @@ def attack_filter():
         return {"result":all_attacks}
 
     elif type1=='endpoint':### 타겟 점검
-        sckt = sckt_utils.create_socket()
-        command = {
-            "type":"web",
-            "command":[{
-                "type":"scan",
-                "src_ip":src_ip,
-                "dst_ip":dst_ip
-            }]
-        }
-        sckt_utils.send_with_size(sckt, bson.dumps(command))
-        recvData = sckt_utils.recv_data(sckt)
-        recvData = bson.loads(recvData)
-        logger.info(f"[ATTACK] ENDPOINT \"scan_result\" : {recvData}")
-        sckt.close()
+        # 포트 스캔 (제거)
+        # sckt = sckt_utils.create_socket()
+        # command = {
+        #     "type":"web",
+        #     "command":[{
+        #         "type":"scan",
+        #         "src_ip":src_ip,
+        #         "dst_ip":dst_ip
+        #     }]
+        # }
+        # sckt_utils.send_with_size(sckt, bson.dumps(command))
+        # recvData = sckt_utils.recv_data(sckt)
+        # recvData = bson.loads(recvData)
+        # logger.info(f"[ATTACK] ENDPOINT \"scan_result\" : {recvData}")
+        # sckt.close()
 
-        # filter by middle category in MySQL
-        filtered_attacks = parser.recv_to_json(recvData)
+        # filtered_attacks = parser.recv_to_json(recvData)
         # res = {"result":filtered_attacks}
         # logger.info(f"[ATTACK] ENDPOINT \"filtered_attacks\" : {res}")
-        return {"result":filtered_attacks}
+
+        attacks = Attack.query.filter(Attack.type=="cve").all()
+        all_attacks = parser.attack_query_to_json(attacks)
+        return {"result":all_attacks}
 
     elif type1=="malware":### endpoint 솔루션
         attacks = Attack.query.filter(Attack.type=="mal").all()
@@ -57,7 +66,15 @@ def attack_filter():
         logger.info(f"[ATTACK] MALWARE \"result\" : {all_attacks}")
         
         return {"result":all_attacks}
-
+    elif type1=="product_packet":
+        service = request.args.get('service')
+        r = os.popen(f'searchsploit {service} -j').read()
+        r = json.loads(r)
+        results = r["RESULTS_EXPLOIT"]
+        results.extend(r["RESULTS_PAPER"])
+        return {
+            "results":results
+        }
 
 
 @bp.route('/start', methods=['POST'])
@@ -116,7 +133,123 @@ def attack_start():
         "status": statusCode.OK
     }
 
+@bp.route('/edit', methods=['POST'])
+def product_packet_edit():
+    if request.method=='GET':
+        logger.warning(f"{loggers.RED}[ATTACK] NOT GET{loggers.END}")
+        return {"status":statusCode.METHOD_ERROR}
+    getFromFront = request.get_data().decode()
+    getFromFront = json.loads(getFromFront)
+    script_number = getFromFront['script_number']
 
+    # searchsploit에서 파일 위치 가져오기
+    ssploit_res = os.popen(f"searchsploit -p {script_number} -j").read()
+    ssploit_res = ssploit_res.split("\n")
+    for i in ssploit_res:
+        if "Path" in i:
+            ssploit_path = i
+            break
+    ssploit_path = ssploit_path.split(":")[1].replace(" ","")
+    new_path = CURRENT_DIR+"/temp/tmp.py"
+    
+    # 해당 파일 temp 밑으로 복사
+    os.system(f"cp {ssploit_path} {new_path}")
+
+    code = os.popen(f"cat {ssploit_path}").read()
+    print(code)
+
+    return {
+        "code": code
+    }
+
+@bp.route("/save", methods=['POST'])
+def product_packet_save():
+    if request.method=='GET':
+        logger.warning(f"{loggers.RED}[ATTACK] NOT GET{loggers.END}")
+        return {"status":statusCode.METHOD_ERROR}
+    getFromFront = request.get_data().decode()
+    getFromFront = json.loads(getFromFront)
+    edited_code = getFromFront['edited']
+    usage = getFromFront["usage"]
+    attack_id = int(getFromFront["attack_id"])
+    src_ip = getFromFront["src_ip"]
+    dst_ip = getFromFront["dst_ip"]
+
+    logger.info(f"attack_id:{attack_id}, src_ip:{src_ip}, dst_ip:{dst_ip}, usage:{usage}")
+
+
+    tmp_path = CURRENT_DIR+"/temp/tmp.py"
+    f = open(tmp_path,"w")
+    f.write(edited_code)
+    f.close()
+    
+    filename = os.popen(f'searchsploit {attack_id} -j').read()
+    filename = json.loads(filename)
+    logger.info(f"filename:{filename}")
+
+    _command = {
+        "type":"web",
+        "command":[
+            {
+                "type" : "defense",
+                "src_ip" : src_ip,
+                "attack_id" : attack_id,
+                "port": 7777,  # 7777 로 하드코딩
+            },
+            {
+                "attack_id": attack_id, # exploit-db 번호
+                "filename": filename["RESULTS_EXPLOIT"][0]["Path"].split("/")[-1],
+                "type": "product_packet", # exploit-db 대신 product_packet 사용
+                "src_ip": src_ip,
+                "dst_ip": dst_ip,
+                "usage": usage,
+                "download": downloadURL, # X로 암호화된 api,
+                "dst_port": 7777,
+            }
+        ]
+    }
+
+
+    sckt = sckt_utils.create_socket()
+    # send command to tcp server
+    sckt_utils.send_with_size(sckt, bson.dumps(_command))
+
+    reportData = sckt_utils.recv_data(sckt) # json
+    reportData = bson.loads(reportData)
+    logger.info(f"[ATTACK] SSPLOIT \"report\" : {reportData}")
+    sckt.close()
+
+    try:
+        pre_no = Report.query.order_by(Report.no.desc()).first().no
+    except:
+        pre_no = -1
+    now = datetime.datetime.now()
+    attack_start_time = now.strftime('%Y-%m-%d %H:%M:%S')
+
+    to_MySQL_result = parser.save_report_to_MySQL(pre_no, attack_start_time, reportData)
+    if to_MySQL_result == "Insert ERROR":
+        logger.warning(f"{loggers.RED}[ATTACK] ERROR while inserting report into MySQL{loggers.END}")
+    
+    return {
+        "status": statusCode.OK
+    }
+
+    # filtered_attacks = parser.recv_to_json(recvData)
+    # res = {"result":filtered_attacks}
+    # logger.info(f"[ATTACK] ENDPOINT \"filtered_attacks\" : {res}")
+
+
+    
+
+    # return {
+    #     "attack_id": attack_id,
+    #     "filename":filename["RESULTS_EXPLOIT"][0]["Path"].split("/")[-1],
+	# 	"type": "exploit-db",
+    #     "src_ip": src_ip,
+    #     "dst_ip": dst_ip,
+    #     "usage": usage,
+    #     "code": downloadURL
+	# }
 
 # 암호화 된
 @bp.route('/download/crypt/<int:attackIdx>/', methods=['GET'])
